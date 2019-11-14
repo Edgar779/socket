734dc735608c43e4d311b39a1ec339d9cf6f5202
root <root@localhost.localdomain>
2019-11-14 14:22:09 -0600

    ok

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const router = express.Router();
-app.use(express.static(path.join(__dirname, '../public')));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    console.log(`Server is up on ${PORT}`);
-})
